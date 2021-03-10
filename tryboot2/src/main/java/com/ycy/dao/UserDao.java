b2af1b1e6cdc22dfdd2694aa6489580847cb3ca8
fzuycy <2253708742@qq.com>
2021-03-10 17:31:53 +0800

    Create UserDao.java

---
+++ tryboot2/src/main/java/com/ycy/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.ycy.dao;
+
+import com.ycy.pojo.User;
+
+public interface UserDao {
+    //根据用户名查询用户
+    User getUserByName(String name);
+    //插入一个用户
+    int addUser(User user);
+    //修改一个用户的信息
+    int updateUser(User user);
+    //删除一个用户
+    int deleteUser(String name);
+}
