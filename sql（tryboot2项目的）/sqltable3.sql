92e637e934af4a9d112b32c44a0b741954f87598
fzuycy <2253708742@qq.com>
2021-03-10 17:35:21 +0800

    Create sqltable3.sql

---
+++ sql（tryboot2项目的）/sqltable3.sql
@@ -0,0 +1,14 @@
+CREATE TABLE myuser(
+ username VARCHAR(32),
+ userpsw VARCHAR(32),
+ usertel VARCHAR(32)
+);
+
+CREATE TABLE shower(
+mytype VARCHAR(32),
+path VARCHAR(32),
+sname VARCHAR(32),
+username VARCHAR(32),
+ischecked VARCHAR(32),
+mydate DATE
+);
