795393c98be06bda393d2cb895fa05dd132e5799
fzuycy <2253708742@qq.com>
2021-03-10 17:31:29 +0800

    Create CorsConfig.java

---
+++ tryboot2/src/main/java/com/ycy/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.ycy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET","HEAD","POST","PUT","DELETE","OPTIONS")
+//                .allowCredentials(true)
+//                .maxAge(3600)
+//                .allowedHeaders("*");
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+//@Override
+//public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//            .allowCredentials(true)
+//            .allowedHeaders("*")
+//            .allowedMethods("*")
+//            .allowedOrigins("*")
+//            .maxAge(3600);
+//}
+}
