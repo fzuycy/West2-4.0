af71b9a6c107d9088fb4f6510915b515b87085f2
fzuycy <2253708742@qq.com>
2021-03-10 17:32:24 +0800

    Create Tryboot2Application.java

---
+++ tryboot2/src/main/java/com/ycy/Tryboot2Application.java
@@ -0,0 +1,13 @@
+package com.ycy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tryboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tryboot2Application.class, args);
+    }
+
+}
