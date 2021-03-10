4e2d627cd6951099f57a60ee74688cd6936b3808
fzuycy <2253708742@qq.com>
2021-03-10 10:45:31 +0800

    Create Tryboot3Application.java

---
+++ tryboot3/src/main/java/com/ycy/Tryboot3Application.java
@@ -0,0 +1,13 @@
+package com.ycy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tryboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tryboot3Application.class, args);
+    }
+
+}
