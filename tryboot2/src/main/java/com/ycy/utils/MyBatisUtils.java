2d50325b8e8654bc987c3512e676b0ea109f2d37
fzuycy <2253708742@qq.com>
2021-03-10 17:32:33 +0800

    Create MyBatisUtils.java

---
+++ tryboot2/src/main/java/com/ycy/utils/MyBatisUtils.java
@@ -0,0 +1,30 @@
+package com.ycy.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisUtils {
+    public static SqlSessionFactory sqlSessionFactory;
+    static {
+        try {
+//            String resource = "org/mybatis/example/mybatis-config.xml";
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static SqlSession getSession(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        return sqlSession;
+    }
+
+}
