198a70de3a9cb5904c26a46ae945f8baeabeaa69
fzuycy <2253708742@qq.com>
2021-03-10 10:45:11 +0800

    Create UserDaoImpl.java

---
+++ tryboot3/src/main/java/com/ycy/dao/UserDaoImpl.java
@@ -0,0 +1,41 @@
+package com.ycy.dao;
+
+import com.ycy.pojo.User;
+import com.ycy.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl {//这边方法的重复部分不少，其实可以统一写成一个方法
+    //通过用户名查询
+    public User selectUserByName(String name){
+        SqlSession session = MyBatisUtils.getSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        User user = userDao.getUserByName(name);
+        System.out.println("我在查询"+user);
+        return user;
+    }
+
+    //存入用户(初步改好)
+    public void saveUser(User user){
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        userDao.addUser(user);
+
+        sqlSession.commit();
+
+        sqlSession.close();
+        System.out.println(user+"保存成功");
+    }
+
+//    //删除用户
+//    public void deleteUser(User user){
+//        SqlSession sqlSession = MyBatisUtils.getSession();
+//        UserDao userDao = sqlSession.getMapper(UserDao.class);
+//        userDao.deleteUser(user.getUsername());
+//        sqlSession.commit();
+//
+//        sqlSession.close();
+//    }
+
+}
